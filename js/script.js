7551a8bb7d55f57203c4b119837766f07ec22df0
Michael A. Agarenzo <michaelagarenzo@mail.adelphi.edu>
2020-03-07 00:27:20 -0500

    Format JS

--- js/script.js
+++ js/script.js
@@ -1,54 +1,54 @@
 function changePage(event, page) {
 
-  if (document.getElementById("wrapper").style.display == "none") {
-    document.getElementById("wrapper").style.display = "block";
-  }
+    if (document.getElementById("wrapper").style.display == "none") {
+        document.getElementById("wrapper").style.display = "block";
+    }
 
-  var sections = document.getElementsByClassName("section");
-  for (var i = 0; i < sections.length; i++) {
-    sections[i].style.display = "none";
-  }
-  document.getElementById(page).style.display = "block";
+    var sections = document.getElementsByClassName("section");
+    for (var i = 0; i < sections.length; i++) {
+        sections[i].style.display = "none";
+    }
+    document.getElementById(page).style.display = "block";
 
-  var pages = document.getElementsByClassName("page");
-  for (var i = 0; i < pages.length; i++) {
-    pages[i].style.backgroundColor = "#007BFF";
-    pages[i].style.color = "#ffffff";
-  }
-  
-  var link = document.getElementById(page + "-link");
-  link.style.backgroundColor = "#ffffff";
-  link.style.color = "#007BFF";
+    var pages = document.getElementsByClassName("page");
+    for (var i = 0; i < pages.length; i++) {
+        pages[i].style.backgroundColor = "#007BFF";
+        pages[i].style.color = "#ffffff";
+    }
 
-  if (screen.width <= 960) {
-    document.getElementById("menu").style.display = "none";
-  }
+    var link = document.getElementById(page + "-link");
+    link.style.backgroundColor = "#ffffff";
+    link.style.color = "#007BFF";
+
+    if (screen.width <= 960) {
+        document.getElementById("menu").style.display = "none";
+    }
 
 }
 
 function toggleMenu() {
 
-  var style = document.getElementById("menu").style.display;
-  if (!style || style == "none") {
-    document.getElementById("menu").style.display = "block";
-    document.getElementById("wrapper").style.display = "none";
-  } else {
-    document.getElementById("menu").style.display = "none";
-    document.getElementById("wrapper").style.display = "block";
-  }
+    var style = document.getElementById("menu").style.display;
+    if (!style || style == "none") {
+        document.getElementById("menu").style.display = "block";
+        document.getElementById("wrapper").style.display = "none";
+    } else {
+        document.getElementById("menu").style.display = "none";
+        document.getElementById("wrapper").style.display = "block";
+    }
 
 }
 
 function showDiv(id) {
 
-  if (document.getElementById(id).style.display == "none") {
-    document.getElementById(id).style.display = "block";
-  } else {
-    document.getElementById(id).style.display = "none";
-  }
+    if (document.getElementById(id).style.display == "none") {
+        document.getElementById(id).style.display = "block";
+    } else {
+        document.getElementById(id).style.display = "none";
+    }
 
 }
 
 $(".carousel").carousel({
-  interval: 30000
+    interval: 30000
 });
