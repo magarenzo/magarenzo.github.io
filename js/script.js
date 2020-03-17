7fed0e4e9b852488ae0c2e54a8c9bd5d3a4033d3
Michael A. Agarenzo <magarenzo@gmail.com>
2020-03-16 20:21:30 -0400

    script.js - Alter showDiv to edit text based on bool

--- js/script.js
+++ js/script.js
@@ -39,12 +39,21 @@ function toggleMenu() {
 
 }
 
-function showDiv(id) {
+function showDiv(id, changeText) {
 
     if (document.getElementById(id).style.display == "none") {
         document.getElementById(id).style.display = "block";
     } else {
         document.getElementById(id).style.display = "none";
+    }
+
+    if (changeText) {
+        var button = document.getElementById(id + "-button");
+        if ((button.innerText) == "Show") {
+            button.innerText = "Hide";
+        } else {
+            button.innerText = "Show";
+        }
     }
 
 }
