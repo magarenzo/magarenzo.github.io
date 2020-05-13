37b7ddd40539a0c43f4f42535adf6defff1fd180
Michael A. Agarenzo <michaelagarenzo@mail.adelphi.edu>
2020-05-12 22:13:07 -0400

    Load div if homepage was redirected to from a subdomain

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 /**
  * Set carousel interval to 30 seconds
+ * Load div if homepage was redirected to from a subdomain
  */
 
 $(".carousel").carousel({
     interval: 30000
 });
+
+if (document.referrer.includes("experience")) {
+    changePage(event, "experience");
+} else if (document.referrer.includes("projects")) {
+    changePage(event, "projects");
+
+} else if (document.referrer.includes("more")) {
+    changePage(event, "more");
+
+}
