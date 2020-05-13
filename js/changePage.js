a2722fb0304212539382d85b7e39c6804077f016
Michael A. Agarenzo <michaelagarenzo@mail.adelphi.edu>
2020-05-13 02:31:17 -0400

    Add ! to logic for referrer

--- js/changePage.js
+++ js/changePage.js
@@ -32,8 +32,7 @@ function changePage(event, page) {
 
     if (page == "home") {
         window.history.pushState("", "", "/");
-        var referrer = document.referrer;
-        if (referrer.match("/^(experience|projects|more)$/")) {
+        if (!document.referrer.match("/^(experience|projects|more)$/")) {
             window.location.reload();
         }
     } else {
