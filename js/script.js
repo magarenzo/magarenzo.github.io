a309d387c55ccbbffa748807a87ee8273130390f
Michael A. Agarenzo <michaelagarenzo@mail.adelphi.edu>
2020-08-03 23:50:53 -0400

    Init and run Firebase

--- js/script.js
+++ js/script.js
@@ -15,3 +15,20 @@ if (document.referrer.includes("experience")) {
     changePage(event, "more");
 
 }
+
+/**
+ * Initialize and run Firebase analytics
+ */
+
+firebase.initializeApp({
+    apiKey: "AIzaSyBS_h8sKsOllQfY7ziQkzMUNsh2dz0akOw",
+    authDomain: "magarenzo-com.firebaseapp.com",
+    databaseURL: "https://magarenzo-com.firebaseio.com",
+    projectId: "magarenzo-com",
+    storageBucket: "magarenzo-com.appspot.com",
+    messagingSenderId: "473158920186",
+    appId: "1:473158920186:web:cd929bda1eee1a893ba643",
+    measurementId: "G-4W9PTW5H04"
+});
+
+firebase.analytics();
