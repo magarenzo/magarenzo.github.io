b7255b5050ac2a070cc5c6f1126f21d894a40f2e
Michael A. Agarenzo <magarenzo@gmail.com>
2020-09-25 15:03:17 -0400

    Done with HTML init; working on ChangePageJS

--- js/changePage.js
+++ js/changePage.js
@@ -6,13 +6,14 @@
 function changePage(event, page) {
 
     var sections = document.getElementsByClassName("section");
-
     for (var i = 0; i < sections.length; i++) {
-
         sections[i].style.display = "none";
-
     }
 
+    var subSections = document.getElementsByClassName("sub-section");
+    // for loop
+
     document.getElementById(page).style.display = "block";
+    // show relevant sub-sections
 
 }
