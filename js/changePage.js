0a2303f7c6e3f35a9b28d3ce2c335d06fb63f909
Michael A. Agarenzo <magarenzo@gmail.com>
2020-09-25 21:29:46 -0400

    Almost done with redesign

--- js/changePage.js
+++ js/changePage.js
@@ -6,14 +6,32 @@
 function changePage(event, page) {
 
     var sections = document.getElementsByClassName("section");
-    for (var i = 0; i < sections.length; i++) {
-        sections[i].style.display = "none";
+    for (var section = 0; section < sections.length; section++) {
+        sections[section].style.display = "none";
     }
 
     var subSections = document.getElementsByClassName("sub-section");
-    // for loop
+    for (var subSection = 0; subSection < subSections.length; subSection++) {
+        subSections[subSection].style.display = "none";
+    }
 
     document.getElementById(page).style.display = "block";
-    // show relevant sub-sections
+
+    var pageSubSections = document.getElementsByClassName(page + "-sub-section");
+    for (var pageSubSection = 0; pageSubSection < pageSubSections.length; pageSubSection++) {
+        pageSubSections[pageSubSection].style.display = "block";
+    }
+
+    if (page == "home") {
+        document.title = "magarenzo.com"
+    } else {
+        document.title = page.charAt(0).toUpperCase() + page.slice(1) + " | magarenzo.com";
+    }
+
+    window.scrollTo(0, 0);
+
+    if (screen.width <= 960) {
+        document.getElementById("button-toggle").click();
+    }
 
 }
